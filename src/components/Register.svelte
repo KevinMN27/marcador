94e2f09e04a60926df704f8ce0b5de67f36d9fab
KevinMN27 <ko739mn@gmail.com>
2024-05-08 01:16:06 -0700

    intento 2

--- src/components/Register.svelte
+++ src/components/Register.svelte
@@ -1,55 +1,54 @@
 <div class="max-w-4xl mx-auto font-[sans-serif] text-[#333] p-6">
 	<div class="text-center mb-16">
 		<a href="javascript:void(0)"
-			><img src="https://readymadeui.com/readymadeui.svg" alt="logo" class="w-52 inline-block" />
+			><img src="https://seeklogo.com/images/H/hecho-en-mexico-logo-F086CFC8CD-seeklogo.com.png" alt="logo" class="w-52 inline-block" />
 		</a>
-		<h4 class="text-base font-semibold mt-3">Sign up into your account</h4>
+		<h4 class="text-xl text-white font-semibold mt-3">Registrate</h4>
 	</div>
 	<form>
 		<div class="grid sm:grid-cols-2 gap-y-7 gap-x-12">
 			<div>
 				<label class="text-sm mb-2 block">Nombre</label>
 				<input
-				class="input" title="Input (text)" type="text" placeholder="Nombre"
+				class="input text-white" title="Input (text)" type="text" placeholder="Nombre"
 				/>
 			</div>
 			<div>
-				<label class="text-sm mb-2 block">Last Name</label>
+				<label class="text-sm mb-2 block">Apellido</label>
 				<input
-				class="input" title="Input (text)" type="text" placeholder="Apellido"
+				class="input text-white" title="Input (text)" type="text" placeholder="Apellido"
 				/>
 			</div>
 			<div>
-				<label class="text-sm mb-2 block">Email Id</label>
+				<label class="text-sm mb-2 block">Correo</label>
 				<input
-				class="input" title="Input (text)" type="text" placeholder="Correo"
+				class="input text-white" title="Input (text)" type="text" placeholder="Correo"
 				/>
 			</div>
 			<div>
-				<label class="text-sm mb-2 block">Mobile No.</label>
+				<label class="text-sm mb-2 block">Telefono</label>
 				<input
-				class="input" title="Input (text)" type="text" placeholder="Telefono"
+				class="input text-white" title="Input (text)" type="text" placeholder="Telefono"
 				/>
 			</div>
 			<div>
-				<label class="text-sm mb-2 block">Password</label>
+				<label class="text-sm mb-2 block">Contraseña</label>
 				<input
-				class="input" title="Input (text)" type="text" placeholder="Contraseña"
+				class="input text-white" title="Input (text)" type="text" placeholder="Contraseña"
 				/>
 			</div>
 			<div>
-				<label class="text-sm mb-2 block">Confirm Password</label>
+				<label class="text-sm mb-2 block">Confirmar Contraseña</label>
 				<input
-				class="input" title="Input (text)" type="text" placeholder="Confirmar contraseña"
+				class="input text-white" title="Input (text)" type="text" placeholder="Confirmar contraseña"
 				/>
 			</div>
 		</div>
 		<div class="!mt-10">
 			<button
-				type="button"
-				class="min-w-[150px] py-3 px-4 text-sm font-semibold rounded text-white bg-primary-500 hover:bg-primary-400 focus:outline-none"
+			type="button" class="btn variant-filled-primary"
 			>
-				Sign up
+				Registrate
 			</button>
 		</div>
 	</form>
