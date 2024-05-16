18d81b02a6f7a4c9b1edf0304687a151bfecbc3f
KevinMN27 <ko739mn@gmail.com>
2024-05-16 10:50:34 -0700

    estoy imbecil

--- src/lib/supabaseConnection.js
+++ src/lib/supabaseConnection.js
@@ -1,6 +1,38 @@
+
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://ozcxexmhtvinzgdfasin.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im96Y3hleG1odHZpbnpnZGZhc2luIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTUyOTY4NjAsImV4cCI6MjAzMDg3Mjg2MH0.yNa0ho3ebHAPAVPxy58eUbBoYPSl1RpUJcCDRQZhB84'
+const supabase = createClient('https://ozcxexmhtvinzgdfasin.supabase.co', 'eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im96Y3hleG1odHZpbnpnZGZhc2luIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTUyOTY4NjAsImV4cCI6MjAzMDg3Mjg2MH0', {
+    global: { fetch: fetch.bind(globalThis) }
+})
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+async function LoadData(){
+
+    
+    try{
+        const { data , error } = await supabase 
+        .from('Partidos')
+        .select('equipo_local_id', 'equipo_visitante_id')
+
+        if (error){
+            console.error('Error fetching data')
+            return;
+        }
+
+        data.forEach(partido => {
+            equipoLocalIds.push(partido.equipo_local_id);
+            equipoVisitanteIds.push(partido.equipo_visitante_id);
+        });
+
+        // Aquí puedes hacer algo con las listas, por ejemplo, imprimir en consola
+        console.log('IDs de equipos locales:', equipoLocalIds);
+        console.log('IDs de equipos visitantes:', equipoVisitanteIds);
+    }   
+    catch(err){
+        console.error('Error inesperado:',err)
+    } 
+}
+
+
+// No olvides invocar la función para que se ejecute
+LoadData();
+
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,4 +1,7 @@
 <!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script src="../supabaseConnection.js">
+	
+</script>
 <section class="h-screen flex flex-col justify-center items-center bg-surface">
 	<div class="flex flex-row m-4">
 		<div class="max-w-sm rounded overflow-hidden shadow-lg">
