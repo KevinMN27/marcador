72c3ee11434025eb8346841deaaeeddb360a9acf
KevinMN27 <ko739mn@gmail.com>
2024-05-18 22:44:59 -0700

    aqui andamos

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -6,12 +6,13 @@
 	let equipos = [];
     let loading = true;
     let errorMessage = '';
+	let teams = [];
 
     async function loadEquipoData() {
         try {
             let { data, error } = await supabase
                 .from('Partidos')
-                .select('*');
+                .select('equipo_local_id, equipo_visitante_id');
 				console.table(data);
 
             if (error) {
@@ -24,7 +25,19 @@
                 equipos = data.map(partido => ({
                     local: partido.equipo_local_id,
                     visitante: partido.equipo_visitante_id
+						
+					
+					
                 }));
+				onMount(() => {
+        loadEquipoData().then(() => {
+            if (equipos.length > 0) {
+                updateHTML(equipos);  // Asegurarse de que esto solo se llama en el cliente.
+            }
+        });
+    });
+				
+				
             } else {
                 errorMessage = "No hay partidos disponibles";
             }
@@ -36,13 +49,30 @@
         }
     }
 
+	function updateHTML(equipos) {
+        const contenedor = document.getElementById('equipo');
+        if (contenedor) {
+            let htmlContent = equipos.map(e => 
+                `<div>Equipo ${e.local} vs Equipo ${e.visitante}</div>`
+            ).join('');
+            contenedor.innerHTML = htmlContent;
+        }
+    }
+
 loadEquipoData();
 </script>
 <section class="h-screen flex flex-col justify-center items-center bg-surface">
 	<div class="flex flex-row m-4">
 		<div class="max-w-sm rounded overflow-hidden shadow-lg">
 			<div class="px-6 py-4">
-				<div id="equipo" class="font-bold text-xl mb-2">Equipo1 vs Equipo 2</div>
+				
+    
+        <div id="equipo" class="font-bold text-xl mb-2">
+            Equipo {teams.local} vs Equipo {teams.visitante}
+        </div>
+    
+
+				
 				<p class="text-white-700 text-base">No tengo idea de como mostrar los datos del fetch aca >:(</p>	
 			</div>
 			<div class="px-6 pt-4 pb-2">
