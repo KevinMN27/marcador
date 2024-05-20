67d9c584f4ea7be2deea60b48891c313909fc3b1
KevinMN27 <ko739mn@gmail.com>
2024-05-20 13:46:02 -0700

    ya salen nombres

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -6,16 +6,35 @@
 	let loading = true;
 	let errorMessage = '';
 	let teams = [];
-
+	let name = [];
+	let matchs = []
+	
 	onMount(() => {
 		async function loadEquipoData() {
 			try {
 				let { data, error } = await supabase
 					.from('Partidos')
 					.select('equipo_local_id, equipo_visitante_id');
-				console.log(data);
+				
 				teams = data;
+					
+					let { data: data_equipos, error: error_local } = await supabase
+						.from('Equipos')
+						.select('id,nombre')
+									
+					data.forEach(match => {
+						const names = [];
 
+						const equipoLocal = data_equipos.find(equipo => equipo.id === match.equipo_local_id);
+						if (equipoLocal) names.push(equipoLocal.nombre);
+
+						const equipoVisitante = data_equipos.find(equipo => equipo.id === match.equipo_visitante_id);
+						if (equipoVisitante) names.push(equipoVisitante.nombre);
+
+						matchs.push(names);
+						matchs = matchs.slice();
+					});
+					
 				if (error) {
 					errorMessage = 'Error cargando los datos';
 					return;
@@ -27,16 +46,17 @@
 		}
 		loadEquipoData();
 	});
+	
 </script>
 
 <section class="h-screen flex flex-col justify-center items-center bg-surface">
 	<!-- Tarjeta -->
-	{#each teams as team}
+	{#each matchs as match}
 		<div class="flex flex-row m-4">
 			<div class="max-w-sm rounded overflow-hidden shadow-lg">
 				<div class="px-6 py-4">
 					<div id="equipo" class="font-bold text-xl mb-2">
-						Equipo {team.equipo_local_id} vs Equipo {team.equipo_visitante_id}
+						{match[0]} vs {match[1]}
 					</div>
 
 					<p class="text-white-700 text-base">
